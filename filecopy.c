399396af144c31a83c01b591e3b6ad3f85002556
Michael Cramer <michael@cramer.name>
2019-02-19 16:57:24 -0800

    Init commit on start

--- filecopy.c
+++ filecopy.c
@@ -18,5 +18,15 @@ int
 main(int argc, char const *argv[])
 {
 
+    char fromFile[255];
+    char toFile[255];
+
+    printf("Welcome to the File Copy Program by Thomas Sechrist!\n");
+    printf("Enter the name of the file to copy from:");
+    scanf("%s", fromFile);
+    printf("Enter the name of the file to copy to: ");
+    scanf("%s", toFile);
+    
+
     return 0;
 }
